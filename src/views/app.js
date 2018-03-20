54f3a050cd551aa9b46a8d14624e896fef344c0f
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-19 20:32:51 -0500

    in progress

--- src/views/app.js
+++ src/views/app.js
@@ -1,14 +1,31 @@
 var AppView = Backbone.View.extend({
-
+  
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
+    // let videoList = new VideoListView(window.exampleVideoData);
+
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    new VideoPlayerView({
+      el: this.$('.players'),
+      model: this.videos.at(0),
+      collection: this.videos
+    })
+
+    new VideoListView({
+      el: this.$('.list'),
+
+      collection: this.videos
+    });
+
+
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,25 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    // this.listenTo(this.collection, 'sync', this.render);
+    this.render(); 
+
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+
+    
+
+
     return this;
   },
+
+  
+
 
   template: templateURL('src/templates/videoList.html')
 
 });
+
